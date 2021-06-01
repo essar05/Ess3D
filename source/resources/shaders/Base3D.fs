0c3c648bf4d6b92d47c1acf1d51eca592ffe7e95
Essar <glaedrthewise@gmail.com>
2021-06-01 14:51:09 +0300

    Test Blender exported model and fix Base3D fragment shader

--- source/resources/shaders/Base3D.fs
+++ source/resources/shaders/Base3D.fs
@@ -8,7 +8,12 @@ in vec2 fragmentUV;
 out vec4 color;
 
 uniform sampler2D material_texDiffuse1;
+uniform bool use_material_texDiffuse1;
 
 void main() {
-  color = texture(material_texDiffuse1, fragmentUV);
+  if (use_material_texDiffuse1) {
+    color = texture(material_texDiffuse1, fragmentUV);
+  } else {
+    color = vec4(0.1, 0.1, 0.1, 1.0);
+  }
 }
